1128555deb1c370b69cd2a017f8953fb1c3cd265
@Kamran.jabiyev <kamran.jabiyev@code.edu.az>
2021-12-09 21:12:45 +0400

    basket

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// let a=5;
+// localStorage.setItem("a",5);
+// console.log(localStorage.getItem("a"));
+// sessionStorage.setItem("b",10);
+// console.log(sessionStorage.getItem("b"));
+
+// localStorage.setItem("a","Nicat")
+
+// localStorage.setItem("arr",JSON.stringify([]));
+// console.log(JSON.parse(localStorage.getItem("arr")));
+// localStorage.removeItem("arr")
+// localStorage.clear()
+// document.cookie="username=Togrul; max-age=30;domain=code.edu.az,secure";
